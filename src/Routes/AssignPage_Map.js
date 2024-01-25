9b1c5b6b0ddf560cf0dd95efde43cb892065bfa8
최원서 <dnjstj88@naver.com>
2024-01-25 11:15:36 +0900

    feat: 카테고리 등록 연결

--- src/Routes/AssignPage_Map.js
+++ src/Routes/AssignPage_Map.js
@@ -15,52 +15,58 @@ import {
 
 function AssignPage_Map() {
   const id_param = useParams().id;
-
-  const [category, setCategory] = useState("");
-  const [categoryList, setCategoryList]= useState(
-    {
-        booth : "부스",
-        amenity: "편의시설",
-        store : ["주점", "푸드트럭"],
-    }
-  );
-  const [type, setType]=useState("");
-  const [boardList, setBoardList] = useState(["대동제 총정리1", "대동제 총정리2", "대동제 총정리3"]);
+  const [category, setCategory] = useState('');
+  const [categoryList, setCategoryList] = useState({
+    booth: '부스',
+    amenity: '편의시설',
+    store: ['주점', '푸드트럭'],
+  });
+  const [type, setType] = useState('');
+  const [boardList, setBoardList] = useState([
+    '대동제 총정리1',
+    '대동제 총정리2',
+    '대동제 총정리3',
+  ]);
   const [currentPage, setCurrentPage] = useState(1);
   const [pageNum, setPageNum] = useState(8);
   const [totalPages, setTotalPages] = useState(4);
   const [itemID, setItemID] = useState(1);
-  const [mode, setMode] = useState("");
+  const [mode, setMode] = useState('');
 
-
-  useEffect(()=>{
-    console.log("type (AssignPage_Map):",type)
-  },[type]);
+  useEffect(() => {
+    console.log(type, category);
+  }, [type]);
   return (
-    <Wrapper style={{height:'auto'}}>
+    <Wrapper style={{ height: 'auto' }}>
       <GlobalStyles />
       <UpperBar_Component />
       <MiddleBar_Component3 text="지도" id_param={id_param} />
 
       <AssignBox>
-      <Assign_List 
-        category={category} setCategory={setCategory}
-        categoryList = {categoryList} type={type} setType={setType}
-        boardList={boardList} setBoardList={setBoardList}
-        currentPage={currentPage} setCurrentPage={setCurrentPage}
-        pageNum={pageNum} totalPages={totalPages}
-        itemid = {itemID} setItemID={setItemID}
-        setMode = {setMode}/>
+        <Assign_List
+          category={category}
+          setCategory={setCategory}
+          categoryList={categoryList}
+          type={type}
+          setType={setType}
+          boardList={boardList}
+          setBoardList={setBoardList}
+          currentPage={currentPage}
+          setCurrentPage={setCurrentPage}
+          pageNum={pageNum}
+          totalPages={totalPages}
+          itemid={itemID}
+          setItemID={setItemID}
+          setMode={setMode}
+        />
 
-        {/* 임시 카테고리 설정 레이아웃 */}
-        <Assign category="store"/>
+        <Assign category={category} type={type} />
       </AssignBox>
     </Wrapper>
   );
 }
 
 export default AssignPage_Map;
-
 
 const AssignBox = styled(Flex)`
   margin-top: 32px;
