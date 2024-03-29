2bdf03b92289644d522a948e9104c23a459c4ce4
노준호 <wnsgh1873@gmail.com>
2024-03-29 12:24:02 +0900

    폰트변경 pull 받기 위함

--- src/GlobalStyles.js
+++ src/GlobalStyles.js
@@ -10,12 +10,6 @@ export const GlobalStyles = createGlobalStyle`
     font-style: normal;
 }
 
-@font-face {
-    font-family: 'Pretendard-Regular';
-    src: url('https://cdn.jsdelivr.net/gh/Project-Noonnu/noonfonts_2107@1.1/Pretendard-Regular.woff') format('woff');
-    font-weight: 400;
-    font-style: normal;
-}
 
 *{
     margin: 0;
