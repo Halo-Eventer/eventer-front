7ca7275a1e730a40f4bb714b325a584ba2b25dbc
최원서 <dnjstj88@naver.com>
2024-04-26 12:28:46 +0900

    fix : 공영주차장 한줄로 수정

--- src/asset/changeMarker.js
+++ src/asset/changeMarker.js
@@ -35,16 +35,17 @@ export const changeMarker = (activeCategory, isPrev, text) => {
     content: `
   <div > 
   <div style="display:flex; justify-content: center;">
-  <div style = "
-   border-radius:28px;display:flex;justify-content:center;align-items: center;">
-  <img style="width:28px;height:32px;" src = ${img}></img>
-  </div>
+    <div style = "
+    border-radius:28px;display:flex;justify-content:center;align-items: center;">
+    <img style="width:28px;height:32px;" src = ${img}></img>
+    </div>
   </div>
   <div style="display:flex; font-size: 12px;
           font-family: 'NanumSquareNeo';
           text-shadow: -1px -1px 0 #fff, 1px -1px 0 #fff, -1px 1px 0 #fff, 1px 1px 0 #fff;
-           font-weight: 800; justify-content: center;line-height: 16px;"> ${text} </div>
-          </div>
+           font-weight: 800; justify-content: center;line-height: 16px; white-space: nowrap"> ${text} 
+           
+          </div> 
   </div>`,
     size: new naver.maps.Size(50, 50),
     origin: new naver.maps.Point(0, 0),
