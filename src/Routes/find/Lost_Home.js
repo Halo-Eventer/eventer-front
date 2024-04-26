9c706b5e8a3eccf6bc2ab7feb5aee9ce55b0ade0
노준호 <wnsgh1873@gmail.com>
2024-04-26 17:46:33 +0900

    분실물리스트 하단바 완성

--- src/Routes/find/Lost_Home.js
+++ src/Routes/find/Lost_Home.js
@@ -5,10 +5,16 @@ import styled from 'styled-components';
 import { Flex } from '../../asset/Style';
 import { useEffect, useState } from 'react';
 import { lostItemGet } from 'apis/apis_GET';
+import xButton from 'asset/images/XButton.svg';
 
 function Lost_Home() {
   const navigate = useNavigate();
   const [data, setData] = useState([]);
+  const [showPopUp, setShowPopUp] = useState(true);
+
+  const onClick_close = () => {
+    setShowPopUp(false);
+  }
 
   useEffect(() => {
     window.scrollTo(0, -200);
@@ -41,7 +47,131 @@ function Lost_Home() {
               </div>
             );
           })}
+           <div>
+                <Box image={xButton}>
+                  <Tag>
+                    <Category>타입</Category>
+                    <Name>이름</Name>
+                  </Tag>
+                </Box>
+              </div>
+              <div>
+                <Box image={xButton}>
+                  <Tag>
+                    <Category>타입</Category>
+                    <Name>이름</Name>
+                  </Tag>
+                </Box>
+              </div>
+              <div>
+                <Box image={xButton}>
+                  <Tag>
+                    <Category>타입</Category>
+                    <Name>이름</Name>
+                  </Tag>
+                </Box>
+              </div>
+              <div>
+                <Box image={xButton}>
+                  <Tag>
+                    <Category>타입</Category>
+                    <Name>이름</Name>
+                  </Tag>
+                </Box>
+              </div>
+              <div>
+                <Box image={xButton}>
+                  <Tag>
+                    <Category>타입</Category>
+                    <Name>이름</Name>
+                  </Tag>
+                </Box>
+              </div>
+              <div>
+                <Box image={xButton}>
+                  <Tag>
+                    <Category>타입</Category>
+                    <Name>이름</Name>
+                  </Tag>
+                </Box>
+              </div>
+              <div>
+                <Box image={xButton}>
+                  <Tag>
+                    <Category>타입</Category>
+                    <Name>이름</Name>
+                  </Tag>
+                </Box>
+              </div>
+              <div>
+                <Box image={xButton}>
+                  <Tag>
+                    <Category>타입</Category>
+                    <Name>이름</Name>
+                  </Tag>
+                </Box>
+              </div>
+              <div>
+                <Box image={xButton}>
+                  <Tag>
+                    <Category>타입</Category>
+                    <Name>이름</Name>
+                  </Tag>
+                </Box>
+              </div>
+              <div>
+                <Box image={xButton}>
+                  <Tag>
+                    <Category>타입</Category>
+                    <Name>이름</Name>
+                  </Tag>
+                </Box>
+              </div>
+              <div>
+                <Box image={xButton}>
+                  <Tag>
+                    <Category>타입</Category>
+                    <Name>이름</Name>
+                  </Tag>
+                </Box>
+              </div>
+              <div>
+                <Box image={xButton}>
+                  <Tag>
+                    <Category>타입</Category>
+                    <Name>이름</Name>
+                  </Tag>
+                </Box>
+              </div>
+              <div>
+                <Box image={xButton}>
+                  <Tag>
+                    <Category>타입</Category>
+                    <Name>이름</Name>
+                  </Tag>
+                </Box>
+              </div>
+              <div>
+                <Box image={xButton}>
+                  <Tag>
+                    <Category>타입</Category>
+                    <Name>이름</Name>
+                  </Tag>
+                </Box>
+              </div>
+              
         </Container>
+
+        {showPopUp
+        &&
+        <PopUpBar>
+        <h1>분실물 습득·분실 시 종합안내센터로 방문해주세요.
+          <br/>상세 위치는 공연장 지도를 참고해주세요.</h1>
+        <h2><span>문의 전화번호</span> : 061-244-8729</h2>
+        <img onClick={onClick_close} src={xButton}/>
+      </PopUpBar>
+        }
+       
       </Wrapper>
     </>
   );
@@ -117,4 +247,46 @@ export const Name = styled.div`
   font-style: normal;
   font-weight: 600;
   line-height: 24px; /* 150% */
+`;
+
+export const PopUpBar = styled.div`
+position:fixed;
+bottom:16px;
+
+width: 358px;
+height: 88px;
+flex-shrink: 0;
+border-radius: 8px;
+background: #222;
+border : 1px solid #555;
+
+display :flex;
+flex-direction:column;
+gap:4px;
+padding:12px 16px;
+h1{
+  color: #FFF;
+  font-family: Pretendard;
+  font-size: 14px;
+  font-style: normal;
+  font-weight: 400;
+  line-height: 20px; /* 142.857% */
+}
+h2{
+  color: #FFF;
+  font-family: Pretendard;
+  font-size: 14px;
+  font-style: normal;
+  font-weight: 400;
+  line-height: 20px;
+  span{
+    font-weight:600;
+  }
+}
+img{
+  position:absolute;
+  top:12px;
+  right:12px;
+  cursor:pointer;
+}
 `;
