0a2772839cc1185782e66267faea28c28e87f45d
노준호 <wnsgh1873@gmail.com>
2024-07-16 16:55:59 +0900

    🎨 Improve : 변수명 리팩토링 완료 및 sitemap 관련 파일 삭제

--- src/utils/InitInfo.js
+++ src/utils/InitInfo.js
@@ -1,21 +1,22 @@
-import { lostItemForm_blank, mapCategoryForm_blank, mapForm_blank, missingForm_blank, missingPersonForm_blank, postForm_blank, urgentForm_blank } from "constants/const_assign";
+import {
+    lostItemFormBlank,
+    mapCategoryFormBlank,
+    mapFormBlank,
+    missingPersonFormBlank,
+    postFormBlank,
+    urgentFormBlank,
+} from 'constants/const_assign';
 
 export const InitInfo = (category, type) => {
     let tmp;
     console.log('category, type in firstInfo : ', category, type);
 
-    if (category === 'notice')
-        tmp = postForm_blank;
-    else if (category === 'store')
-        tmp = mapCategoryForm_blank
-    else if (category === 'lost')
-        tmp = lostItemForm_blank;
-    else if (category === 'missingPerson')
-        tmp = missingPersonForm_blank;
-    else if (category === 'urgent')
-        tmp = urgentForm_blank;
-    else
-        tmp = mapForm_blank;
+    if (category === 'notice') tmp = postFormBlank;
+    else if (category === 'store') tmp = mapCategoryFormBlank;
+    else if (category === 'lost') tmp = lostItemFormBlank;
+    else if (category === 'missingPerson') tmp = missingPersonFormBlank;
+    else if (category === 'urgent') tmp = urgentFormBlank;
+    else tmp = mapFormBlank;
 
-    return {...tmp, type:type};
+    return { ...tmp, type: type };
 };
